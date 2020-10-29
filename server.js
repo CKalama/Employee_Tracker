95ca0f3345f82074c274ba05d3a312ddacc0bfad
Chris Kalama <chriskalama96@gmail.com>
2020-10-28 22:28:08 -0500

    Created viewAll Function, nested three if statements into with connection.query to SELECT FROM the selected table. Console.logged each if statement to test. Manipulated third if statement to only pull title and salary for testing purposes.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const { allowedNodeEnvironmentFlags } = require('process');
 // const { SlowBuffer } = require('buffer');
 
 var connection  = mysql.createConnection({
@@ -9,16 +10,23 @@ var connection  = mysql.createConnection({
     password:"password",
     database:'department_db'
 });
+connection.connect(function(err) {
+    if(err) {
+        console.error("error connecting " + err.stack);
+        return;
+    }
+    console.log("connected" + connection.threadId);
+});
 
 
-var answers = {
-    title: 'Frontend',
-    salary: 60000,
-    department_id: 2
-}
+// var answers = {
+//     title: 'Frontend',
+//     salary: 60000,
+//     department_id: 2
+// }
 
 
-function mainQs() {
+function mainQs(data) {
     inquirer.prompt ([
         {
             message:"What Would You Like to Do?",
@@ -27,16 +35,55 @@ function mainQs() {
             choices:["Add Department", "Add Role", "Add Employee", "View Departments", "View Employee", "View Role", "Update Employee"]
         }
     ]).then(function(answers) {
+        //Mayve make this a Switch command
         console.log("Answers!", answers);
 
-        if(answers) {
-            
+        if(answers.whichCommand === "Add Department") {
+
+            addDepartment(answers);
+        } else if (answers.whichCommand === "Add Role") {
+
+            addRole(answers);
+        } else if (answers.whichCommand === "Add Employee") {
+
+            addEmployee(answers);
+        } else if (answers.whichCommand === "View Departments") {
+
+            viewAll(answers);
+        } else if (answers.whichCommand === "View Employee") {
+
+            viewAll(answers);
+        } else if(answers.whichCommand === "View Role") {
+
+            viewAll(answers);
+        } else if (answers.whichCommand === "Update Employee") {
+
+            updateEmployee(answers);
         }
-
-
-
     })
 }
+
+function viewAll(placeHolder) {
+    if (placeHolder.whichCommand === "View Departments") {
+    connection.query("SELECT * FROM department", function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        }) 
+    };
+    if (placeHolder.whichCommand === "View Employee") {
+        connection.query("SELECT * FROM employee", function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        })
+    };
+    if (placeHolder.whichCommand === "View Role") {
+        connection.query(`SELECT title, salary FROM role = ${placeHolder.title}`, function(err, result) {
+            if(err) throw err; 
+            //console.log(result);
+            console.log("Job Title and Salary:", result);
+        })
+    }
+};
 
 // SELECT FROM for View Departments, 
 // INSERT INTO For Adds 
