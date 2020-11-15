6a7c47aebc6825b0d3d6cef5ea9dc852f7386f35
Chris Kalama <chriskalama96@gmail.com>
2020-11-15 01:23:38 -0600

    found a bug, in pseudocode on tables.sql

--- tables.sql
+++ tables.sql
@@ -12,7 +12,7 @@ id INT NOT NULL AUTO_INCREMENT,
 title VARCHAR(30) NULL,
 salary DECIMAL NULL,
 department_id INT NULL,
-PRIMARY KEY(id)
+PRIMARY KEY(role_id)
 );
 
 CREATE TABLE employee (
@@ -22,4 +22,7 @@ role_id INT NULL,
 manager_id INT NULL,
 
 PRIMARY KEY(id)
+KEY `employee_ibfk_1` (role_id)
 );
+
+-- I need to find out a foreign key issue, where it allows me to add a department, but I cant put a new employee inside the new department. I think it has something to do with a foreign key not reading the correct table it is supposed to.
